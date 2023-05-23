f6ab2cfa3f7978d4159cf9d36022391bb0bd8234
Michał Szyma <devszyma@gmail.com>
2023-05-23 22:54:54 +0200

    Add license ISC; Add icon to the app

---
+++ README.md
@@ -0,0 +1,26 @@
+# Chat GPT App
+
+The application is a wrapper for the web version of Chat GPT.
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+`npm start`
+
+### Compiles and minifies for production
+
+`npm run builld`
+
+## Icons
+
+Source: https://uxwing.com/chatgpt-icon
+
+## license
+
+- **[ISC license](https://opensource.org/license/isc-license-txt)**
+- Copyright © 2023, Michał Szyma
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const nativeImage = require('electron').nativeImage
 const path = require('path')
 
 function createWindow () {
@@ -6,13 +7,14 @@ function createWindow () {
     width: 1900,
     height: 1000,
     title: "ChatGPT",
-    icon: path.join(__dirname, 'icon.png'),
+    // icon: path.join(__dirname, 'icon', 'Icon-512x512.png'),
     webPreferences: {
-      nodeIntegration: false,
+      nodeIntegration: true,
       contextIsolation: true
     }
   })
-
+  console.log(path.join(__dirname, 'icon', 'Icon-512x512.png'))
+  // win.setIcon(path.join(__dirname, 'icon.png'));
   win.on('page-title-updated', (evt) => {
     evt.preventDefault();
   });
